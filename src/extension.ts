03b6670054bf1112f75922aef280adedbae93d8a
sosuisen <hidekazu.kubota@gmail.com>
2025-02-07 21:35:17 +0900

    feat: add CodeLens for generate builder class

--- src/extension.ts
+++ src/extension.ts
@@ -282,6 +282,85 @@ class ControllerSupportLensProvider implements vscode.CodeLensProvider {
 	}
 }
 
+class BuilderClassCodeLensProvider implements vscode.CodeLensProvider {
+	private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
+	public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event;
+
+	constructor() {
+		// カーソル位置が変更されたときにCodeLensを更新
+		vscode.window.onDidChangeTextEditorSelection(() => {
+			this._onDidChangeCodeLenses.fire();
+		});
+	}
+
+	async provideCodeLenses(document: vscode.TextDocument): Promise<vscode.CodeLens[]> {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor || editor.document !== document) {
+			return [];
+		}
+
+
+		const cursorPosition = editor.selection.active;
+		const cursorLine = editor.selection.active.line;
+		// カーソル行の内容
+		const cursorLineText = document.lineAt(cursorLine).text;
+		if (!cursorLineText.includes(' new ')) {
+			return [];
+		}
+
+		// カーソル位置の前後の文字を取得
+		const textBeforeCursor = document.getText(new vscode.Range(
+			cursorPosition.line,
+			Math.max(0, cursorPosition.character - 1),
+			cursorPosition.line,
+			cursorPosition.character
+		));
+		const textAfterCursor = document.getText(new vscode.Range(
+			cursorPosition.line,
+			cursorPosition.character,
+			cursorPosition.line,
+			cursorPosition.character + 1
+		));
+
+		// アルファベットまたは数字かどうかを判定する正規表現
+		const alphaNumericPattern = /[a-zA-Z0-9]/;
+
+		// カーソル位置の前後のいずれかの文字がアルファベットまたは数字であるかを判定
+		if (!alphaNumericPattern.test(textBeforeCursor) && !alphaNumericPattern.test(textAfterCursor)) {
+			return [];
+		}
+
+		// 上記のチェック後でないと、vscode.executeTypeDefinitionProviderでワーニングが出る。
+
+		// カーソル位置の型定義を取得
+		const typeDefinitions = await vscode.commands.executeCommand<vscode.Location[]>(
+			'vscode.executeTypeDefinitionProvider',
+			document.uri,
+			cursorPosition
+		);
+
+		// javafx.scene.* のクラスのnewが見つかった場合のみCodeLensを表示
+		if (typeDefinitions && typeDefinitions.length > 0
+			&& typeDefinitions[0].uri.path.includes('javafx.scene.')
+		) {
+			const range = new vscode.Range(
+				cursorLine,
+				0,
+				cursorLine,
+				0
+			);
+
+			return [new vscode.CodeLens(range, {
+				title: 'Generate Builder Class',
+				command: 'javafx-controller-support.generateBuilderClass'
+			})];
+		}
+
+		return [];
+
+	}
+}
+
 function checkAllFxmlFiles() {
 	vscode.workspace.findFiles("src/**/*.fxml").then(files => {
 		files.forEach(uri => {
@@ -435,7 +514,7 @@ ${indentUnit}}
 	);
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('javafx-controller-support.showMethods', async () => {
+		vscode.commands.registerCommand('javafx-controller-support.generateBuilderClass', async () => {
 			const editor = vscode.window.activeTextEditor;
 			if (!editor) {
 				vscode.window.showErrorMessage('エディタが開かれていません。');
@@ -675,6 +754,13 @@ ${indentUnit}}
 	);
 
 	context.subscriptions.push(fxmlWatcher);
+
+	context.subscriptions.push(
+		vscode.languages.registerCodeLensProvider(
+			{ language: 'java', scheme: 'file' },
+			new BuilderClassCodeLensProvider()
+		)
+	);
 }
 
 const diagnosticCollection = vscode.languages.createDiagnosticCollection('fxml');
